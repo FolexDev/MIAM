3b3a798a657f62ef9c2dda3a121f07e567f93754
FolexDev <alex@mediaonegroup.ch>
2026-05-13 19:42:24 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,12 +1,13 @@
 const CACHE = 'miam-v1';
-const ASSETS = ['/', '/MIAM/', '/MIAM/index.html'];
-
+const ASSETS = ['/', '/MIAM/', '/MIAM/index.html', '/MIAM/MIAM Icon 192.png', '/MIAM/MIAM Icon 512.png'];
+ 
 self.addEventListener('install', e => {
   e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)));
 });
-
+ 
 self.addEventListener('fetch', e => {
   e.respondWith(
     caches.match(e.request).then(r => r || fetch(e.request))
   );
 });
+ 
